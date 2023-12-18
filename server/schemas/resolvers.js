80767b023477ae24396d069b37f95167e4c2a0f0
Janet Webster <twixmixy@gmail.com>
2023-12-18 14:00:17 -0500

    Remove CategoryMenu component and update dependencies

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
