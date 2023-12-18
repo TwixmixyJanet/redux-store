abca2160e9435120c49bc671aef33d19eb182702
Janet Webster <twixmixy@gmail.com>
2023-12-18 14:30:08 -0500

    Add useProductReducer function to reducers.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,5 @@
+import { useReducer } from 'react';
+
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -91,3 +93,7 @@ export const reducer = (state, action) => {
       return state;
   }
 };
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
